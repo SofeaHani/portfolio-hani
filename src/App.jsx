0ebcab6f9dd741640f039c9fcadb454bb740abaf
Sofea Hani <sofeahani02@gmail.com>
2025-07-28 17:12:00 +0800

    Update cert 3

--- src/App.jsx
+++ src/App.jsx
@@ -139,8 +139,7 @@ function App() {
 
           <div className="cert-card">
             <img src="GraphicDesign.jpeg" alt="Project Z" />
-            <h4 style={{ marginTop: '16px' }}>Graphic Design Basic of UIUX <br /> Certificate</h4>
-            <p>Description Project 3</p>
+            <h4 style={{ marginTop: '16px' }}>Introduction to Graphic Design Basic of UIUX <br /> Certificate</h4>
             <div className="buttons">
               <a href="GRAPHIC DESIGN Basic of UIUX Certificate.pdf" className="cert-btn">Certificate</a>
             </div>
